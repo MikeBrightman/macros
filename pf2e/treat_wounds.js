513db5e409934e84370d1f3b95af4e0cb0d10026
MikeBrightman <Mikebrightman+GitHub@gmail.com>
2020-11-09 17:52:51 +0000

    Initial revision of Treat Wounds

--- pf2e/treat_wounds.js
+++ pf2e/treat_wounds.js
@@ -1,130 +1,120 @@
-/* This is a script that automates the Treat Wounds or Battle Medicine skill. It returns 3 (or 2) chat messages:
-    1. A shout-out
-    2. The result of the medicine check
-    3. The result according to the degree of succes of the medicine check (no message if the check is a failure)
+/* 
+  This is a script that rolls a selected actor's medicine against a "Teat Wounds" roll.
+  It outputs two roll results into the chat log:
+  1. The level of medicine check and result of the medicine roll by the selected actor
+  2. The resulting hitpoint changes according to the degree of succes (even if zero)
   
-  You can use key combinations for different DC's, which will also add the bonus healing corresponding to that DC. 
-  The key combinations are:
-    - normal click for Trained DC
-    - ctrl-click for Expert DC
-    - alt-click for Master DC
-    - ctrl-alt-click for Legendary DC
-  For example, if you use ctrl-click, you will roll a med check for the Expert DC (20) and, if you will succed, you will add a bonus healing of 10 to the 2d8 roll.
+  Holding down keys when you run/click the macro changes the Difficulty Class (DC) of the roll.
+  Hold down the corresponding keys for a different roll
+    - None: Trained, DC 15, no additional healing
+    - Ctrl: Expert, DC 20, 10 additional healing
+    - Alt: Master, DC 30, 30 additional healing
+    - Ctrl+Alt: Legendary, DC 40, 50 additional healing
   
   For this to work, you need to have your character token selected.
+
+  Developed using rules found at https://2e.aonprd.com/Actions.aspx?ID=57 as of the 9th of November, 2020
+  Please check this page if you are preparing to use treat wounds often, and want a better understanding
+  or if you have questions about the roll itself (or DM/Player discussion).
+
+  TODO:
+  Take into account Medic Dedication Feat (additional healing effect)
+  Take into account Feats such as Natural medicine (use alternate proficiency bonus)
+  Factor in other bonuses such as 
+    item (e.g. expanded healer's tools, Serene Mutagen)
+    status (e.g. Heroism 3rd level Spell, Competitive Edge Focus Spell) 
+    circumstance (e.g. Risky Surgery Feat + deal damage before healing, 
+                 Fresh Ingredients Herbalist Dedication Skill Feat + item cost)
 */
 
 
-// Variables used for different DC's
+// Outcome prefix text for each case
+let Success_flavor_msg = "Success, you will heal:";
+let Failure_flavor_msg = "Failure";
+let Dmg_flavor_msg = "Damage taken:";
 
-let bonusheal = 0;
-let dc_text = " - Trained DC (15)";
-let dc_value = 0;
-
-
-// Flavor text that will be displayed in the chat box. You can change the text according to your own will, be creative and have fun!
-
-let Shoutout_flavor_msg = "I will try to heal you!";
-let Success_flavor_msg = "Success!";
-let CritSuccess_flavor_msg = "Outstanding Success!";
-let Failure_flavor_msg = "Failure...";
-let CritFailure_flavor_msg = "Or maybe... I'll be the one to kill you";
-let Heal_flavor_msg = "You shall heal for:";
-let Dmg_flavor_msg = "You will take this damage:";
-
-
-// Here the DC variables will be changed according to which key combinations you use
-
-if(event.ctrlKey && event.altKey == true) {
-  bonusheal = 50;
-  dc_text = " - Legendary DC (40)";
-  dc_value = 25;
-} else if(event.ctrlKey) {
-  bonusheal = 10;
-  dc_text = " - Expert DC (20)";
-  dc_value = 5;
-} else if(event.altKey) {
-  bonusheal = 30;
-  dc_text = " - Master DC (30)";
-  dc_value = 15;
+// Select difficulty class + determine bonus to result based on keys held
+let dc_text = "(Trained, DC 15)";
+let dc_mod = 0
+if(event.ctrlKey) {
+  if(event.altKey) {
+    dc_text = "(Legendary, DC 40)";
+    dc_mod = 25;
+  }
+  else
+  {
+    dc_text = "(Expert, DC 20)";
+    dc_mod = 5;
+  }
 }
+else if(event.altKey) {
+  dc_text = "(Master, DC 30)";
+  dc_mod = 15;
+}
+let BonusHealFromDC = dc_mod*2;
+let failThresh = 14+dc_mod;
 
+// Roll the outcome
+let skillRoll = new Roll("1d20+@skills.med.value", token.actor.getRollData());
+skillRoll.roll();
+skillRoll.toMessage({
+  flavor: "Treat wounds "+dc_text,
+  speaker: ChatMessage.getSpeaker({token: token})
+});
 
-// Declaring the dice rolls
+let result = skillRoll.total; //final result with modifiers
+let crit = skillRoll.result[0] == 20; //0 or 1 respresenting a crit roll
 
-let r1 = new Roll("1d20+@skills.med.value", token.actor.getRollData());
-let r2 = new Roll('2d8+@bonusheal', {bonusheal});
-let r3 = new Roll('4d8+@bonusheal', {bonusheal});
-let r4 = new Roll("1d8");
-
-
-// Shout-out your intention to heal
-
-ChatMessage.create({content: Shoutout_flavor_msg+dc_text, speaker: ChatMessage.getSpeaker({actor: actor})});
-
-
-// Rolling the medicine check
-
-r1.roll();
-let a = r1.total;
-
+let successRoll = new Roll('2d8+@bonusheal', {BonusHealFromDC});
+let critSuccessRoll = new Roll('4d8+@bonusheal', {BonusHealFromDC});
+let failureRoll = new Roll("0d0");
+let critFailureRoll = new Roll("1d8");
 
 // Comparing the medicine check with the DC and returning the corresponding result
-
-if(a>14+dc_value) { 
-  if(a-token.actor.data.data.skills.med.value==1) {
-    if (a-10>14+dc_value) {
-      r1.toMessage({
-        flavor: Success_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-      r2.roll();
-      r2.toMessage({
-        flavor: Heal_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-    } else {
-      r1.toMessage({
-        flavor: Failure_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-       });
-      }
-  } else if(a-10>14+dc_value || a-token.actor.data.data.skills.med.value == 20) { 
-      r1.toMessage({
-        flavor: CritSuccess_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-      r3.roll();
-      r3.toMessage({
-        flavor: Heal_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-  } else {
-        r1.toMessage({
-          flavor: Success_flavor_msg,
-          speaker: ChatMessage.getSpeaker({token: token}),
-        });
-        r2.roll();
-        r2.toMessage({
-          flavor: Heal_flavor_msg,
-          speaker: ChatMessage.getSpeaker({token: token}),
-        });
-      }
-} else if(a<15+dc_value) {
-    if (a+10<15+dc_value || a-token.actor.data.data.skills.med.value==1) {
-      r1.toMessage({
-        flavor: CritFailure_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-      r4.roll();
-      r4.toMessage({
-        flavor: Dmg_flavor_msg,
-        speaker: ChatMessage.getSpeaker({token: token}),
-      });
-    } else {
-         r1.toMessage({
-          flavor: Failure_flavor_msg,
-          speaker: ChatMessage.getSpeaker({token: token}),
-         });
-        }
+let hitPointRoll;
+let outcomeText = "";
+if(result>failThresh)
+{
+    outcomeText +=Success_flavor_msg;
+    hitPointRoll = successRoll;
+    if(result-10>failThresh)
+    {
+        outcomeText = "Critical " + outcomeText;
+        hitPointRoll = critSuccessRoll;
+    }
 }
+else
+{
+    outcomeText += Failure_flavor_msg;
+    hitPointRoll = failureRoll;
+    if(failThresh-9>result)
+    {
+        outcomeText = "Critical " + outcomeText;
+        hitPointRoll = critFailureRoll;
+    }
+}
+
+switch (successLevel) {
+  case 0:
+    //crit failure
+    day = "Sunday";
+    break;
+  case 1:
+    //crit failure
+    day = "Sunday";
+    break;
+  case 2:
+    //crit failure
+    day = "Sunday";
+    break;
+  case 3:
+    //crit failure
+    day = "Sunday";
+    break;
+}
+
+hitPointRoll.roll();
+hitPointRoll.toMessage({
+    flavor: "result "+ result + ", " + outcomeText,
+    speaker: ChatMessage.getSpeaker({token: token})
+  });
